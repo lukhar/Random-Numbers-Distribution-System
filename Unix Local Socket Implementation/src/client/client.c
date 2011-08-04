5a39916cff4bd222e9cc30b98ce233c89e6799c0
Lukasz Haratym <lukasz.har@gmail.com>
2011-08-04 17:34:39 +0200

    add time measure for Socket implementations

--- Unix Local Socket Implementation/src/client/client.c
+++ Unix Local Socket Implementation/src/client/client.c
@@ -37,14 +37,19 @@ int main(int argc, char **argv)
     client_address.sun_family = AF_LOCAL;
     strncpy(client_address.sun_path, socket_path, sizeof(client_address.sun_path) - 1);
 
+    struct timeval starttime, endtime, connection_open_time, data_transfer_time, connection_close_time;
+    gettimeofday(&starttime, 0x0);
     if (connect(socket_fd, (struct sockaddr*) &client_address, sizeof(client_address)) < 0)
     {
         perror("client connect()");
         exit(EXIT_FAILURE);
     }
+    gettimeofday(&endtime, 0x0);
+	timeval_subtract(&connection_open_time, &endtime, &starttime);
 
     sequence_size = calculate_sequence_size(sequence_length);
 
+    gettimeofday(&starttime, 0x0);
     if (write(socket_fd, &sequence_size, sizeof sequence_size) < 0) {
         perror("client write()");
         exit(EXIT_FAILURE);
@@ -56,7 +61,6 @@ int main(int argc, char **argv)
     }
 
     random_bit_sequence = (int8_t*) calloc(sequence_size, sizeof(int8_t));
-//    time_t start = time(0);
     for (int i = 0; i < transfer_amount; ++i) {  
         if (readn(socket_fd, random_bit_sequence, sizeof(int8_t) * sequence_size) < 0) {
             perror("client read()");
@@ -67,9 +71,24 @@ int main(int argc, char **argv)
         //}
         //printf("\n");
     }    
-    time_t stop = time(0);
+    gettimeofday(&endtime, 0x0);
+	timeval_subtract(&data_transfer_time, &endtime, &starttime);
 
-//    printf("elaspsed time %f\n", difftime(stop, start));
+    gettimeofday(&starttime, 0x0);
+    if (close(socket_fd) < 0) {
+        perror("client close()");
+    }
+    gettimeofday(&endtime, 0x0);
+	timeval_subtract(&connection_close_time, &endtime, &starttime);
+
+    printf("%d %d.%d %d.%d %d.%d\n", 
+            sequence_size,
+            connection_open_time.tv_sec, 
+            connection_open_time.tv_usec, 
+            data_transfer_time.tv_sec, 
+            data_transfer_time.tv_usec, 
+            connection_close_time.tv_sec,
+            connection_close_time.tv_usec);
 
     free(random_bit_sequence);
 
--- Unix Socket Implementation/src/client/client.c
+++ Unix Socket Implementation/src/client/client.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
     if (argc == 5) {
         transfer_amount = atoi(argv[4]);
     }
-    printf("transfer_amount = %d\n", transfer_amount);
+    // printf("transfer_amount = %d\n", transfer_amount);
 
     ip_adress = argv[1];
     port = atoi(argv[2]);
@@ -44,19 +44,20 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    struct timeval starttime, endtime, timediff;
-    gettimeofday(&starttime,0x0);
+    struct timeval starttime, endtime, connection_open_time, data_transfer_time, connection_close_time;
+    gettimeofday(&starttime, 0x0);
     if (connect(socket_fd, (struct sockaddr*) &client_address, sizeof(client_address)) < 0)
     {
         perror("client connect()");
         exit(EXIT_FAILURE);
     }
-    gettimeofday(&endtime,0x0);
-	timeval_subtract(&timediff,&endtime,&starttime);
-    printf("%d %d\n", timediff.tv_sec, timediff.tv_usec);
+    gettimeofday(&endtime, 0x0);
+	timeval_subtract(&connection_open_time, &endtime, &starttime);
+
 
     sequence_size = calculate_sequence_size(sequence_length);
 
+    gettimeofday(&starttime, 0x0);
     if (write(socket_fd, &sequence_size, sizeof sequence_size) < 0) {
         perror("client write()");
         exit(EXIT_FAILURE);
@@ -68,21 +69,34 @@ int main(int argc, char **argv)
     }
 
     random_bit_sequence = (int8_t*) calloc(sequence_size, sizeof(int8_t));
-    time_t start = time(0);
     for (int i = 0; i < transfer_amount; ++i) {  
         if (readn(socket_fd, random_bit_sequence, sizeof(int8_t) * sequence_size) < 0) {
             perror("client read()");
             exit(EXIT_FAILURE);
         }
-
-        for (int i = 0; i < sequence_size; i++) {
-            printf("%d", random_bit_sequence[i]);
-        }
-        printf("\n");
+//        for (int i = 0; i < sequence_size; i++) {
+//            printf("%d", random_bit_sequence[i]);
+//        }
+//        printf("\n");
     }
-    time_t stop = time(0);
+    gettimeofday(&endtime, 0x0);
+	timeval_subtract(&data_transfer_time, &endtime, &starttime);
 
-    printf("elaspsed time %f\n", difftime(stop, start));
+    gettimeofday(&starttime, 0x0);
+    if (close(socket_fd) < 0) {
+        perror("client close()");
+    }
+    gettimeofday(&endtime, 0x0);
+	timeval_subtract(&connection_close_time, &endtime, &starttime);
+
+    printf("%d %d.%d %d.%d %d.%d\n", 
+            sequence_size,
+            connection_open_time.tv_sec, 
+            connection_open_time.tv_usec, 
+            data_transfer_time.tv_sec, 
+            data_transfer_time.tv_usec, 
+            connection_close_time.tv_sec,
+            connection_close_time.tv_usec);
 
     free(random_bit_sequence);
 
