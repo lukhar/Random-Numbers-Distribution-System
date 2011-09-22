a903e59466cee107dec630623394f779e86aa486
Lukasz Haratym <lukasz.har@gmail.com>
2011-09-22 22:42:44 +0200

    new data colected for multiplatform system

--- Multiplatform System/mClient/PerformTestsForLogCapture.sh
+++ Multiplatform System/mClient/PerformTestsForLogCapture.sh
@@ -10,7 +10,8 @@ DUMP="tshark -i eth0 -f 'port ${PORT}' -w ${TMP}"
 #DUMP="sudo tcpdump -i eth0 'tcp[tcpflags] & (tcp-syn|tcp-fin|tcp-ack) != 0' -w ${TMP}"
 echo $DUMP
 
-FILTER="tshark -r ${TMP} -R '((tcp.flags == 0x02) || (tcp.flags == 0x12) ) || ((tcp.flags == 0x10) && (tcp.ack==1) && (tcp.len==0)) || (tcp.flags==0x11 || ((tcp.flags == 0x10) && (tcp.ack==10) && (tcp.len==0)))' "
+# waiting for closing socket ACK field with value 13 not 10 why ????
+FILTER="tshark -r ${TMP} -R '((tcp.flags == 0x02) || (tcp.flags == 0x12) ) || ((tcp.flags == 0x10) && (tcp.ack==1) && (tcp.len==0)) || (tcp.flags==0x11 || ((tcp.flags == 0x10) && (tcp.ack==10 || tcp.ack ==14 || tcp.ack==13) && (tcp.len==0)))' "
 
 for amount in `seq 50 50 500`
 do
